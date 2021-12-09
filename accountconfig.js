7123d2999a3fbc5843c933e5c7af4d63291b0254
eachLee <eachli@163.com>
2021-12-09 17:47:43 +0800

    修改账户信息

--- accountconfig.js
+++ accountconfig.js
@@ -1,3 +1,3 @@
 export const CloudEnvId = '云开发环境 id'; // 云开发环境 id
-export const showApiKey = '万维易源 key 值'; // 万维易源 key 值
-export const showApiAppId = '万维易源 appid 值'; // 万维易源 appid 值
+export const showApiKey = '78cbad2432834169a3f7831e9b2cf1bc'; // 万维易源 key 值
+export const showApiAppId = '855984'; // 万维易源 appid 值
